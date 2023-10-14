fcd2fb2ce5e570f9296aad15ab54ca1055f4f591
Goran Ehrsson <goran@technipelago.se>
2023-10-14 10:01:26 +0200

    fix: Sleep 5 minutes (for auto-restart container)

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM eclipse-temurin:17.0.8_7-jre-jammy
 
 ARG UID=1005
-ARG GID=1005
+ARG GID=100
 ARG HOME=/opt/weather
 ENV VISUALIZER_HOME=$HOME
 ENV VISUALIZER_CONFIG=$VISUALIZER_HOME/config
@@ -11,12 +11,13 @@ ENV VISUALIZER_REMOTE="user@server.com:/var/wwwroot/weather"
 ENV VISUALIZER_OPTS="-Djava.util.logging.config.file=$VISUALIZER_CONFIG/visualizer-logging.properties"
 
 RUN apt-get update && apt-get -y -qq install unzip rsync ssh
-RUN groupadd -g $GID weather && useradd -M -u $UID -g weather weather
+#RUN groupadd -g $GID users
+RUN useradd -m -u $UID -g $GID weather
 
-ADD ./entrypoint.sh $VISUALIZER_HOME/entrypoint.sh
 ADD ./build/distributions/visualizer.zip /tmp/visualizer.zip
+ADD ./entrypoint.sh $VISUALIZER_HOME/entrypoint.sh
 RUN mkdir -p $VISUALIZER_HOME && unzip -d $VISUALIZER_HOME /tmp/visualizer.zip && rm -f /tmp/visualizer.zip
-RUN chown -R weather:weather $VISUALIZER_HOME && chmod 750 $VISUALIZER_HOME/entrypoint.sh
+RUN chown -R ${UID}:${GID} $VISUALIZER_HOME && chmod 750 $VISUALIZER_HOME/entrypoint.sh
 
 USER $UID
 WORKDIR $VISUALIZER_HOME
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+umask 002
+
+echo "HOME=$HOME"
 echo "VISUALIZER_HOME=$VISUALIZER_HOME"
 echo "VISUALIZER_CONFIG=$VISUALIZER_CONFIG"
 echo "VISUALIZER_TEMPLATES=$VISUALIZER_TEMPLATES"
@@ -9,10 +12,30 @@ echo "VISUALIZER_OUTPUT=$VISUALIZER_OUTPUT"
 echo "VISUALIZER_OPTS=$VISUALIZER_OPTS"
 
 ls -l $VISUALIZER_HOME
-ls -l $VISUALIZER_HOME/visualizer
-ls -l $VISUALIZER_HOME/config
-ls -l $VISUALIZER_HOME/templates
+ls -ld $VISUALIZER_HOME/visualizer
+ls -ld $VISUALIZER_HOME/config
+ls -ld $VISUALIZER_HOME/templates
 
 $VISUALIZER_HOME/visualizer/bin/visualizer $VISUALIZER_OUTPUT $VISUALIZER_TEMPLATES
 
-rsync -i $VISUALIZER_CONFIG/id_rsa -rltp --chmod=g+r,o+r $VISUALIZER_OUTPUT/ $VISUALIZER_REMOTE
+mkdir -p $HOME/.ssh
+chmod 755 $HOME/.ssh
+cp $VISUALIZER_CONFIG/id_rsa* $HOME/.ssh
+cp $VISUALIZER_CONFIG/known_hosts $HOME/.ssh
+chmod 600 $HOME/.ssh/id_rsa
+chmod 644 $HOME/.ssh/id_rsa.pub
+chmod 644 $HOME/.ssh/known_hosts
+
+ls -l $HOME/.ssh
+
+echo "Uploading..."
+
+rsync -vv -rltp --chmod=g+r,o+r $VISUALIZER_OUTPUT/ $VISUALIZER_REMOTE
+
+echo "Finished uploading, waiting..."
+
+sleep 300
+
+echo "Done"
+
+exit 0
